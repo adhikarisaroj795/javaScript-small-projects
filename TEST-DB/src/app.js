38d13d9759a41e4b52b49df32f786fc2f11ac09b
saroj adhikari <adhikarisaroj291@gmail.com>
2023-03-27 17:15:29 +0545

    creating connection with server

---
+++ TEST-DB/src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+require("./db/conn");
+const app = express();
+const port = process.env.PORT || 3000;
+
+//setting the path
+const staticpath = path.join(__dirname, "../public");
+
+//middleware
+app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
+app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
+
+app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
+app.use(express.static(staticpath));
+app.set("view engine", "hbs");
+
+//routing
+//app.get(path, callback)
+app.get("/",(req, res) =>{
+    res.render("index");
+})
+app.get("/contact",(req, res) =>{
+    res.render("Contact");
+})
+
+//server create
+app.listen(port, () => {
+    console.log(`server is running at port no ${port}`);
+})
---
+++ TEST-DB/src/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+//creating database
+mongoose.connect("mongodb://localhost:27017/sarojdynamic", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("connection successful");
+}).catch((error) => {
+  console.log(error);
+});
