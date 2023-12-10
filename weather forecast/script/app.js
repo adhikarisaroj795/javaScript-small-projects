082a62eeaff218a360f4f5026c3ed6d3ff9aa71f
saroj adhikari <adhikarisaroj291@gmail.com>
2023-12-10 14:59:11 +0545

    completing weather app

--- weather forecast/script/app.js
+++ weather forecast/script/app.js
@@ -1,15 +1,16 @@
 const apiKey = "b0904fa397c2fb7030f60677b8314f57";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon")
 
 
 async function checkweather(city){
-    const response = await fetch(apiUrl + city + '&appid=${apiKey}');
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 if(response.status == 404){
     document.querySelector(".error").style.display = "block"
     document.querySelector(".weather").style.display = "non"
+    console.log(data);
 }else{
     var data = await response.json();
     document.querySelector(".city").innerHTML = data.name;
@@ -19,19 +20,19 @@ if(response.status == 404){
 
 
     if(data.weather[0].main =="Clouds"){
-        weatherIcon.src = "imags/clouds.png";
+        weatherIcon.src = "images/clouds.png";
     }
     else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "imags/rain.png";
+        weatherIcon.src = "images/clear.png";
     }
     else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "imags/rain.png";
+        weatherIcon.src = "images/rain.png";
     }
     else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "imags/drizzle.png";
+        weatherIcon.src = "images/drizzle.png";
     }
     else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "imags/mist.png";
+        weatherIcon.src = "images/mist.png";
     }
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
