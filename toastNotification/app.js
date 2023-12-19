a09f15e01957e90efc60aa88088fe58e0955f307
saroj adhikari <adhikarisaroj291@gmail.com>
2023-12-19 17:57:40 +0545

    adding toast msg

---
+++ toastNotification/app.js
@@ -0,0 +1,29 @@
+let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error';
+let invalidMSg = '<i class="fa-solid fa-circle-exclamation"></i> invalid input, check again';
+
+
+
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+
+    }
+    if(msg.includes('invalid')){
+        toast.classList.add('invalid');
+        
+    }
+    
+setTimeout(()=>{
+    toast.remove();
+},6000);
+
+
+}
