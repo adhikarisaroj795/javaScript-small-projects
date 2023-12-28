c5a388f53151852728d8b39ffc045ce3d2530916
saroj adhikari <adhikarisaroj291@gmail.com>
2023-12-28 19:16:57 +0545

    adding dark theme to website

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -11,3 +11,13 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click',(
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }))
+
+var icon = document.getElementById("micon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "img/sun.png";
+    }else{
+        icon.src = "img/moon.png";
+    }
+}
